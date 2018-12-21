37a46a3803f7d4df17d943f825a14ce57c506784
Daniel L <43019017+funkedee@users.noreply.github.com>
2018-12-20 19:28:32 -0700

    movie-this running

--- liri.js
+++ liri.js
@@ -37,17 +37,46 @@ function concertThis() {
     // api call to bands in town
     axios.get("https://rest.bandsintown.com/artists/" + term + "/events?app_id=codingbootcamp")
     .then(function(res) {
+        // append command to log.txt
+        fs.appendFile("log.txt", "\n>>>" + command + " " + term);
+
+        // loop through concerts
         res.data.forEach(show => {
+            // store concert data
             var info ="\nArtists: " + show.lineup.join(", ") +
             "\nLocation: " + show.venue.name + ", " + show.venue.city + ", " + show.venue.region +
             "\nDate: " + moment(show.datetime).format("MM/DD/YYYY")+
             "\n\n--------------------------------------";
+            
+            // log data and append to log.txt
             console.log(info);
-            fs.appendFile("log.txt", command + "\n" + info);
+            fs.appendFile("log.txt", "\n" + info);
         
         });
     })
 };
+
+function movieThis() {
+    // set default search term
+    if (term === ""){
+        term = "Mr.nobody"
+    };
+
+    // api call to omdb
+    axios.get("http://www.omdbapi.com/?t=" + term + "&apikey=trilogy")
+    .then(function(res){
+        // store movie data
+        var info =  "\nTitle: " + res.data.Title + "\nYear: " + res.data.Year +
+        "\nIMDB: "+ res.data.imdbRating + "\nRotten Tomatoes: " + res.data.Ratings[1].Value +
+        "\nCountry: " + res.data.Country + "\nLaunguage: " + res.data.Language +
+        "\nPlot: " + res.data.Plot + "\nActors: " + res.data.Actors +
+        "\n\n--------------------------------------";
+
+        // log data and append to log.txt
+        console.log(info);
+        fs.appendFile("log.txt", "\n>>>" + command + " " + term + "\n" + info);
+    })
+}
 
 start();
 
